b025a05bdd9f2b75b80a94e192ea022100ca1a04
alexey-kurbetyev <alexeykurbetyev.photo@gmail.com>
2020-07-20 16:39:18 +0300

    homework05

--- python_lesson_05.py
+++ python_lesson_05.py
@@ -1,46 +1,41 @@
 def sum_values(my_dictionary):
+    return sum(my_dictionary.values())
 
-    sum_values = my_dictionary.values()
-    return sum(sum_values)
+print(sum_values({10: 1, 100: 2, 1000: 3}))
 
-print(sum_values({10:1, 100:2, 1000:3}))
 
 def sum_even_keys(my_dictionary):
-    even = 0
-    for value in my_dictionary.values():
-        if value % 2 == 0:
-            even += value
-            return even
-
-
+    values = 0
+    for even in my_dictionary.values():
+        if even % 2 == 0:
+            values += even
+    return values
 
 print(sum_even_keys({1: 5, 2: 2, 3: 3}))
 
+
 def add_ten(my_dictionary):
-    for  key,value in my_dictionary.items():
-            my_dictionary[key] = value + 10
+    for key, value in my_dictionary.items():
+        my_dictionary[key] = value + 10
+    return my_dictionary
+print(add_ten({1:10, 2:20, 3:30}))
 
-            return my_dictionary
-
-print(add_ten({10:1, 100:2, 1000:3}))
 
 def values_that_are_keys(my_dictionary):
-    list = []
+    keys = []
     for value in my_dictionary.values():
-        for key in my_dictionary.keys():
-            if key == value:
-                list.append(key)
-    return list
+      for key in my_dictionary.keys():
+         if key == value:
+           keys.append(key)
+    return keys
 
 print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 
+
 def max_key(my_dictionary):
-    max_k = (max (my_dictionary.values()))
+    maximum = max(my_dictionary.values())
     for key in my_dictionary:
-        if my_dictionary[key] == max_k:
+        if my_dictionary[key] == maximum:
             return key
 
-print(max_key({1:100, 2:2000, 3:4, 4:10}))
-
-
-
+print(max_key({1: 100, 2: 10000, 3: 4, 4: 1000000}))
