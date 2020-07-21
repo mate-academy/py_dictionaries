1bf7c467494282a40e8af603beb6c9468bfbe152
ksushaQA2014 <67373274+ksushaQA2014@users.noreply.github.com>
2020-07-21 11:44:40 +0300

    Update python_dict.py

--- python_dict.py
+++ python_dict.py
@@ -6,13 +6,13 @@
 Write a function named sum_values that takes a dictionary named my_dictionary as a parameter.
 The function should return the sum of the values of the dictionary
 """
-# Write your sum_values function here:
-
-# Uncomment these function calls to test your sum_values function:
-# print(sum_values({"milk":5, "eggs":2, "flour": 3}))
-# should print 10
-# print(sum_values({10:1, 100:2, 1000:3}))
-# should print 6
+def sum_values(my_dictionary):
+    sum_dict = 0
+    for value in my_dictionary.values():
+        sum_dict = sum_dict + value
+    return sum_dict
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+print(sum_values({10:1, 100:2, 1000:3}))
 
 
 """
@@ -22,12 +22,16 @@ as a parameter. This function should return the sum of the values of all even ke
 """
 
 
-# Write your sum_even_keys function here:
+def sum_even_keys(my_dictionary):
+    even_keys = 0
+    for key, values in my_dictionary.items():
+        if key % 2 == 0:
+            even_keys += values
+    return even_keys
 
-# Uncomment these function calls to test your  function:
-# print(sum_even_keys({1:5, 2:2, 3:3}))
+print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
-# print(sum_even_keys({10:1, 100:2, 1000:3}))
+print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
 
 
@@ -36,12 +40,14 @@ as a parameter. This function should return the sum of the values of all even ke
 Create a function named add_ten that takes a dictionary with integer values named my_dictionary as a parameter. 
 The function should add 10 to every value in my_dictionary and return my_dictionary
 """
-# Write your add_ten function here:
+def add_ten(my_dictionary):
+    for values in my_dictionary.keys():
+        my_dictionary[values] = my_dictionary[values] + 10
+    return my_dictionary
 
-# Uncomment these function calls to test your  function:
-# print(add_ten({1:5, 2:2, 3:3}))
+print(add_ten({1:5, 2:2, 3:3}))
 # should print {1:15, 2:12, 3:13}
-# print(add_ten({10:1, 100:2, 1000:3}))
+print(add_ten({10:1, 100:2, 1000:3}))
 # should print {10:11, 100:12, 1000:13}
 
 
