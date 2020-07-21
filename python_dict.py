da7cab7ced45491c14128e30812a2c21388e32fb
ksushaQA2014 <gryshyna.ksusha@gmail.com>
2020-07-21 13:39:04 +0300

    Gryshyna Oksana Dictionaries

--- python_dict.py
+++ python_dict.py
@@ -57,7 +57,7 @@ Create a function named values_that_are_keys that takes a dictionary named my_di
 This function should return a list of all values in the dictionary that are also keys.
 """
 # Write your values_that_are_keys function here:
-
+aaa
 # Uncomment these function calls to test your  function:
 # print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
 # should print [1, 4]
