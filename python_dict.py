0e68387c0f56601d0974dc11220158425ef88d27
MaxUtrobin <ma.utrobin@gmail.com>
2020-07-22 16:31:09 +0300

    My commmmit

--- python_dict.py
+++ python_dict.py
@@ -3,44 +3,59 @@
 Write a function named sum_values that takes a dictionary named my_dictionary as a parameter.
 The function should return the sum of the values of the dictionary
 """
+
 # Write your sum_values function here:
 
+def sum_values(my_dictionary):
+    return sum(my_dictionary.values())
+
 # Uncomment these function calls to test your sum_values function:
-# print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
 # should print 10
-# print(sum_values({10:1, 100:2, 1000:3}))
+print(sum_values({10:1, 100:2, 1000:3}))
 # should print 6
 
 
 """
 2. EvenKeys
-Create a function called sum_even_keys that takes a dictionary named my_dictionary, with all integer keys and values, 
+Create a function called sum_even_keys that takes a dictionary named my_dictionary,
+with all integer keys and values,
 as a parameter. This function should return the sum of the values of all even keys.
 """
 
-
 # Write your sum_even_keys function here:
 
+def sum_even_keys(my_dictionary):
+    sum = 0
+    for key in my_dictionary.keys():
+        if key % 2 == 0:
+            sum += my_dictionary[key]
+            return sum
 # Uncomment these function calls to test your  function:
-# print(sum_even_keys({1:5, 2:2, 3:3}))
+print(sum_even_keys({1:5, 2:2, 3:3}))
 # should print 2
-# print(sum_even_keys({10:1, 100:2, 1000:3}))
+print(sum_even_keys({10:1, 100:2, 1000:3}))
 # should print 6
 
 
 """
 3. Add Ten
-Create a function named add_ten that takes a dictionary with integer values named my_dictionary as a parameter. 
+Create a function named add_ten that takes a dictionary with integer values 
+named my_dictionary as a parameter. 
 The function should add 10 to every value in my_dictionary and return my_dictionary
 """
 # Write your add_ten function here:
 
-# Uncomment these function calls to test your  function:
-# print(add_ten({1:5, 2:2, 3:3}))
-# should print {1:15, 2:12, 3:13}
-# print(add_ten({10:1, 100:2, 1000:3}))
-# should print {10:11, 100:12, 1000:13}
+def add_ten(my_dictionary):
+    for x, item in my_dictionary.items():
+        my_dictionary[x] = item + 10
+    return my_dictionary
 
+# Uncomment these function calls to test your  function:
+print(add_ten({1:5, 2:2, 3:3}))
+# should print {1:15, 2:12, 3:13}
+print(add_ten({10:1, 100:2, 1000:3}))
+# should print {10:11, 100:12, 1000:13}
 
 """
 4. Values That Are Keys
